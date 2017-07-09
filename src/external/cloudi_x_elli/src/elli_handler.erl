91318362b49165fad5fae3021d60afe4e696b9c8
mgpld 111432 <mgpld@free.fr>
2017-07-09 12:26:47 +0200

    Upstream version

--- src/external/cloudi_x_elli/src/elli_handler.erl
+++
@@ -1,6 +0,0 @@
--module(elli_handler).
--include("elli.hrl").
-
--callback handle(Req :: #req{}, callback_args()) ->
-    ignore | {response_code(), [tuple()], binary()} | {ok, [tuple()], binary()}.
--callback handle_event(Event :: elli_event(), Args :: [term()], Config :: [tuple()]) -> ok.
